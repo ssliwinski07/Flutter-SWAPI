72a237da467e08acd6501781620f75d77394ea87
Szymon Śliwiński <122596933+ssliwinski07@users.noreply.github.com>
2025-03-05 20:05:21 +0100

    updated readme.md

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:connector_module/services/getters/services_getters.dart';
+import 'package:connector_module/services/getters/app_dependencies_getter.dart';
 import 'package:ui_module/root.dart';
 
 import 'utils/helpers/consts/consts.dart';
@@ -10,6 +10,6 @@ void main() {
   // set env parameter with prodEnv or mockEnv to init with prod/mock dependencies
   // by defualt env=prodEnv
   // if env=prodEnv counter will increase by 1, if env=mockEnv counter will increase by 4
-  ServicesGetters().setupDependencies(env: mockEnv);
+  AppDependenciesGetter().setupDependencies();
   runApp(const Root());
 }
